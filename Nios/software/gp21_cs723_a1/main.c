7806666cbc2589557efd06d0cb5e299195a0bdb6
Logan Kenwright <lken274@aucklanduni.ac.nz>
2020-03-29 13:19:06 +1300

    added file stubs for tasks

--- Nios/software/gp21_cs723_a1/main.c
+++ Nios/software/gp21_cs723_a1/main.c
@@ -40,6 +40,9 @@ unsigned int getsem_task1_got_sem = 0;
 unsigned int getsem_task2_got_sem = 0;
 char sem_owner_task_name[20];
 
+void getsem_task1(void *pvParameters);
+void getsem_task2(void *pvParameters);
+
 // Local Function Prototypes
 int initOSDataStructs(void);
 int initCreateTasks(void);
@@ -70,34 +73,6 @@ void print_status_task(void *pvParameters)
 	}
 }
 
-// The next two task compete for a shared resource via a semaphore.  The name of
-// the task that owns the semaphore is copied into the global variable
-// sem_owner_task_name[].
-void getsem_task1(void *pvParameters)
-{
-	while (1)
-	{
-		xSemaphoreTake(shared_resource_sem, portMAX_DELAY);
-		// block forever until receive the mutex
-		strcpy(&sem_owner_task_name[0], "getsem_task1");
-		getsem_task1_got_sem++;
-		xSemaphoreGive(shared_resource_sem);
-		vTaskDelay(100);
-	}
-}
-
-void getsem_task2(void *pvParameters)
-{
-	while (1)
-	{
-		xSemaphoreTake(shared_resource_sem, portMAX_DELAY);
-		// block forever until receive the mutex
-		strcpy(&sem_owner_task_name[0], "getsem_task2");
-		getsem_task2_got_sem++;
-		xSemaphoreGive(shared_resource_sem);
-		vTaskDelay(130);
-	}
-}
 
 // The following task fills up a message queue with incrementing data.  The data
 // is not actually used by the application.  If the queue is full the task is
