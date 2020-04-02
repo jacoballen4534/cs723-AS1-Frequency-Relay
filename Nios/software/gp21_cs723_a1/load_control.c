7ceda26a39c623f14d0406d578cda8c2ae215622
Logan Kenwright <lken274@aucklanduni.ac.nz>
2020-04-02 19:52:41 +1300

    added some brief comments describing each file

---
+++ Nios/software/gp21_cs723_a1/load_control.c
@@ -0,0 +1,4 @@
+//This combines the load shedder output and the wall switch output
+//in a "logical AND" fashion, and updates the relevant output LEDs
+//Controls load status LEDs, as well as load shedder LEDs
+//Can disable input from load shedder based on maintenace mode
