8294208ce1d6e96c44b9b8edc4c566f5dfd1f6fa
Jacob <jacoballen4534@gmail.com>
2020-05-06 18:09:24 +1200

    Only accept one switch change at a time. Only update part of previousRawSwitchValue so there is still a diff on the next check

--- shared/wall_switch.c
+++ shared/wall_switch.c
@@ -18,7 +18,7 @@ uint32_t wallSwitchNotificationValue = WALL_SWITCH_NOTIFICATION;
 
 void vWallSwitchFrequencyTask(void *pvParameters);
 void handleTaskCreateError(BaseType_t taskStatus, char *taskName);
-void intToArray(uint8_t * buf, uint32_t input, uint32_t array_length);
+void intToArray(uint8_t *buf, uint32_t input, uint32_t array_length);
 
 int initWallSwitches(void)
 {
@@ -34,7 +34,7 @@ int initWallSwitches(void)
     return 0;
 }
 
-void intToArray(uint8_t * buf, uint32_t input, uint32_t array_length)
+void intToArray(uint8_t *buf, uint32_t input, uint32_t array_length)
 {
     uint32_t mask = 0x0001;
     int i;
@@ -46,30 +46,43 @@ void intToArray(uint8_t * buf, uint32_t input, uint32_t array_length)
 }
 
 void vWallSwitchFrequencyTask(void *pvParameters)
-{   
+{
     uint32_t previousRawSwitchValue = 0;
     while (1)
     {
         uint32_t rawSwitchValue = IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE);
 
-        if (rawSwitchValue != previousRawSwitchValue)
+        if (rawSwitchValue != previousRawSwitchValue) //There has been a change in switches
         {
+
+            uint32_t diff = previousRawSwitchValue ^ rawSwitchValue; // Find the difference in switches
+
             xSemaphoreTake(xSwitchMutex, portMAX_DELAY);
-            intToArray(switchVal, rawSwitchValue, NUM_LOADS);
-            xSemaphoreGive(xSwitchMutex);
-            
-            uint32_t diff = previousRawSwitchValue ^ rawSwitchValue;
             uint8_t i;
-            for(i = 0; i < NUM_LOADS; i++)
+            for (i = 0; i < NUM_LOADS; i++)
             {
-                if (CHECK_BIT(diff, i)) 
+                if (CHECK_BIT(diff, i)) // Only accept the first switch change (Can only send 1 change at a time through queue)
                 {
+                    // Update switchVal to reflect this single change
+                    if (CHECK_BIT(rawSwitchValue, i))
+                        switchVal[i] = 1;
+                    else
+                        switchVal[i] = 0;
+
                     wallSwitchNotificationValue = WALL_SWITCH_NOTIFICATION + i;
                     break;
                 }
             }
-            previousRawSwitchValue = rawSwitchValue;
-            
+            xSemaphoreGive(xSwitchMutex);
+
+            // Update previousRawSwitchValue with the change that has been accepted.
+            // If multiple switches have been changed, the next check will still show a diff.
+            if (CHECK_BIT(rawSwitchValue, i))
+                previousRawSwitchValue |= (1 << i);
+            else
+                previousRawSwitchValue &= ~(1 << i);
+
+            // Send the change
             BaseType_t result = xQueueSend(loadControlNotifyQ, (void *)&wallSwitchNotificationValue, WALL_SWITCH_TASK_TIMEOUT);
             if (result == errQUEUE_FULL)
             {
